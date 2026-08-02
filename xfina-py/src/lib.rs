b28a111d44beab19529dadd0b89669de858496fa
Sakthi Priyan H <sakthipriyans@gmail.com>
2026-08-02 07:29:50 +0530

    refactor: rename packages to xfina across JS and Python ecosystems

--- xfina-py/src/lib.rs
+++ xfina-py/src/lib.rs
@@ -2,7 +2,7 @@ use pyo3::prelude::*;
 
 /// A Python module implemented in Rust for xfina.
 #[pymodule]
-fn xfina_py(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
+fn xfina(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(version, m)?)?;
     Ok(())
 }
